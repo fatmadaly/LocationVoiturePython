af0eb93b1580b2917fcedac4192f12523f609229
fatmadaly <fatmadaly17@gmail.com>
2021-12-08 00:04:47 +0100

    clean the code a bit

--- admin.py
+++ admin.py
@@ -1,26 +1,25 @@
-from tkinter import *
-import tkinter
-window = Tk()
+import tkinter as tk
+window = tk.Tk()
 window.title("2F")
-lbl=Label(window, text="2F System", fg='red', font=("Helvetica", 16))
+lbl=tk.Label(window, text="2F System", fg='red', font=("Helvetica", 16))
 lbl.place(x=100, y=80)
 
 # You will create two text labels namely 'username' and 'password' and and two input labels for them
 
-tkinter.Label(window, text = "Username").grid(row = 0) #'username' is placed on position 00 (row - 0 and column - 0)
+tk.Label(window, text = "Username").grid(row = 0) #'username' is placed on position 00 (row - 0 and column - 0)
 
 # 'Entry' class is used to display the input-field for 'username' text label
-username_input = tkinter.Entry(window) # first input-field is placed on position 01 (row - 0 and column - 1)
+username_input = tk.Entry(window) # first input-field is placed on position 01 (row - 0 and column - 1)
 username_input.grid(row = 0, column = 1)
 
 
-tkinter.Label(window, text = "Password").grid(row = 1) #'password' is placed on position 10 (row - 1 and column - 0)
+tk.Label(window, text = "Password").grid(row = 1) #'password' is placed on position 10 (row - 1 and column - 0)
 
 
-password_input = tkinter.Entry(window) #second input-field is placed on position 11 (row - 1 and column - 1)
+password_input = tk.Entry(window) #second input-field is placed on position 11 (row - 1 and column - 1)
 password_input.grid(row = 1, column = 1)
 # 'Checkbutton' class is for creating a checkbutton which will take a 'columnspan' of width two (covers two columns)
-tkinter.Checkbutton(window, text = "Keep Me Logged In").grid( columnspan = 5 ) 
+tk.Checkbutton(window, text = "Keep Me Logged In").grid( columnspan = 5 ) 
 
 
 def onClick():
@@ -30,8 +29,12 @@ def onClick():
     
     
     
-btn=Button(window, text="Open", fg='blue',command= onClick)
+btn=tk.Button(window, text="Open", fg='blue',command= onClick)
 btn.place(x=120, y=150)                
 window.geometry("300x200+10+10")
 
 window.mainloop()
+
+
+
+
